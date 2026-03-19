4e79d603629eb0d56e11e189a1900ddec362d17c
Mahbubur Rahman <mahbubur.rahman@bevycommerce.com>
2026-03-19 11:29:37 +0600

    added baseURL for server, client temp_fix

--- apps/web/src/lib/axios.ts
+++ apps/web/src/lib/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const baseURL =
+	typeof window === "undefined" ? `${process.env.API_URL}/api/v1` : `/api/v1`;
+
 export const api = axios.create({
 	withCredentials: true,
-	baseURL: `/api/v1`,
+	baseURL,
 });
