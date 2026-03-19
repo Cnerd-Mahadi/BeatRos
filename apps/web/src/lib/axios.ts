97224d836e3583776d962bab58ee815d5758ab98
Mahbubur Rahman <mahbubur.rahman@bevycommerce.com>
2026-03-19 11:13:56 +0600

    temp_fix for avoiding cookie block

--- apps/web/src/lib/axios.ts
+++ apps/web/src/lib/axios.ts
@@ -1,7 +1,6 @@
-import { env } from "@/env";
 import axios from "axios";
 
 export const api = axios.create({
 	withCredentials: true,
-	baseURL: `${env.NEXT_PUBLIC_API_URL}/api/v1`,
+	baseURL: `/api/v1`,
 });
